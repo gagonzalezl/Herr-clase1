be55c28aa97c8aa36837cacbb2ab590a554b77f2
Gabriel Augusto Gonzalez Lozano <gagonzalezl@unal.edu.co>
2021-02-26 11:59:38 -0500

    hola papucho

---
+++ ejemplo.cpp
@@ -0,0 +1,10 @@
+#include<iostream>
+
+ int main(int argc, char  **argv) {
+   int x = 0;
+   for (size_t i = 0; i < 10; i++) {
+    std::cout << x << "hola mundo" << '\n';
+    x++;
+   }
+  return 0;
+}
